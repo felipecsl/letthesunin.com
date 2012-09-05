3f0529ec553c3fe2d3ae0dac921257145155c2c7
Felipe Lima <felipe.lima@gmail.com>
2012-09-04 22:58:34 -0700

    Adding new images

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,29 @@
 $(function() {
   $(".scramble").sortable();
   $(".scramble img").disableSelection();
-  $('.scramble').balloon({ 
+  
+  $('.mobility_scramble, .cloud_scramble').balloon({ 
     offsetX: -340,
+    showAnimation: function(d) { this.fadeIn(d); },
+    classname: 'tip'
+  });
+  $('.context_scramble').balloon({ 
+    offsetX: -285,
+    showAnimation: function(d) { this.fadeIn(d); },
+    classname: 'tip'
+  });
+  $('.social_scramble').balloon({ 
+    offsetX: -235,
+    showAnimation: function(d) { this.fadeIn(d); },
+    classname: 'tip'
+  });
+  $('.collaboration_scramble').balloon({ 
+    offsetX: -405,
+    showAnimation: function(d) { this.fadeIn(d); },
+    classname: 'tip'
+  });
+  $('.optimized_scramble').balloon({ 
+    offsetX: -385,
     showAnimation: function(d) { this.fadeIn(d); },
     classname: 'tip'
   });
